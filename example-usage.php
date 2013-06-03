76e574f9260cae0f9fc00ed56b9b6da5f3f37c42
Dotan Cohen <github-com-2010.04@dotancohen.com>
2013-06-03 09:08:45 +0300

    Provide example usage for ensure_fields() function.

--- example-usage.php
+++ example-usage.php
@@ -1,8 +1,23 @@
 <?php
 
+// Was:
 
-	if ( ( !ensure_fields($document, 'provider') ) {
-		return FALSE;
-	}
+if ( isset($_POST['name']) && $_POST['name']!='' && isset($_POST['password']) && $_POST['password']!='' ) {
+	login();
+} else {
+	send_error();
+}
+
+
+
+// Now:
+
+if ( ensure_fields($_POST, 'name', 'password') ) {
+	login();
+} else {
+	send_error();
+}
+
+
 
 ?>
