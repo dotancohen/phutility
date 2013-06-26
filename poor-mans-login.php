e4b2aaee44fd5159bcc6480e8a9f20a9fc8ec735
Dotan Cohen <github-com-2010.04@dotancohen.com>
2013-06-26 19:40:06 +0300

    Add poor-mans-login

---
+++ poor-mans-login.php
@@ -0,0 +1,27 @@
+<?php
+
+// No database, no session!
+
+$usernameSha1 = 'fde3b9e88dca861202148dea3a7cec8b05befbc9';
+$passwordSha1 = '85987fb0a90e77a0bdeeb5a2026fa8a4a67137f6';
+
+$isAllowedIn  = FALSE;
+
+if ( isset($_POST['username']) || isset($_POST['password']) ) {
+
+	setcookie('login', 'garbage', 0);
+
+	if ( sha1($_POST['username'])==$usernameSha1 && sha1($_POST['password'])==$passwordSha1 ) {
+		// Correct login credentials, set cookie and set pass variable
+		$sevenDays = time() + 7*24*60*60;
+		setcookie('login', sha1($_SERVER['HTTP_USER_AGENT']), $sevenDays);
+		$_COOKIE['login'] = sha1($_SERVER['HTTP_USER_AGENT']);
+		$isAllowedIn = TRUE;
+	}
+
+} else if ( $_COOKIE['login']==sha1($_SERVER['HTTP_USER_AGENT']) ) {
+	$isAllowedIn = TRUE;
+}
+
+
+?>
