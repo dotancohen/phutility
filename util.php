4f79862c4c2e3768b04abc981e5a1d8573ca35b6
Dotan Cohen <github-com-2010.04@dotancohen.com>
2013-06-30 19:00:02 +0300

    Added output_select_options()

--- util.php
+++ util.php
@@ -442,4 +442,32 @@ function get_typical_results($input)
 
 
 
+/**
+* Output the <options> of a select statement and optionally preselect one
+*
+* @author Dotan Cohen
+* @version 2013-06-30
+*
+* @param array   $data        Associative array to be converted to <options>
+* @param string  $quote_char  Character to use to quote values. Must be " or '
+* @param string  $selected    Default value to select
+*
+* @return TRUE
+*/
+function output_select_options($data, $quote_char='"', $selected=NULL)
+{
+	if ( !is_array($data) || !in_array($quote_char, array('"', "'")) ) {
+		return NULL;
+	}
+
+	foreach ( $data as $f=>$v) {
+		$s = $f==$selected ? " selected={$quote_char}selected{$quote_char}" : '';
+		echo "<option value={$quote_char}{$f}{$quote_char}{$s}>{$v}</option>";
+	}
+
+	return TRUE;
+}
+
+
+
 ?>
