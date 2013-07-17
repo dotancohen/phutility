6197277ded56ed43b7bbb5d17d0aae9e5ccdd371
Dotan Cohen <github-com-2010.04@dotancohen.com>
2013-07-17 08:06:44 +0300

    poor-mans-login: Abstract $sercureIdentifier.

--- poor-mans-login.php
+++ poor-mans-login.php
@@ -6,6 +6,7 @@ $usernameSha1 = 'fde3b9e88dca861202148dea3a7cec8b05befbc9';
 $passwordSha1 = '85987fb0a90e77a0bdeeb5a2026fa8a4a67137f6';
 
 $isAllowedIn  = FALSE;
+$secureIdentifier = sha1($_SERVER['HTTP_USER_AGENT']);
 
 if ( isset($_POST['username']) || isset($_POST['password']) ) {
 
@@ -14,12 +15,12 @@ if ( isset($_POST['username']) || isset($_POST['password']) ) {
 	if ( sha1($_POST['username'])==$usernameSha1 && sha1($_POST['password'])==$passwordSha1 ) {
 		// Correct login credentials, set cookie and set pass variable
 		$sevenDays = time() + 7*24*60*60;
-		setcookie('login', sha1($_SERVER['HTTP_USER_AGENT']), $sevenDays);
-		$_COOKIE['login'] = sha1($_SERVER['HTTP_USER_AGENT']);
+		setcookie('login', $secureIdentifier, $sevenDays);
+		$_COOKIE['login'] = $secureIdentifier;
 		$isAllowedIn = TRUE;
 	}
 
-} else if ( $_COOKIE['login']==sha1($_SERVER['HTTP_USER_AGENT']) ) {
+} else if ( $_COOKIE['login']==$secureIdentifier ) {
 	$isAllowedIn = TRUE;
 }
 
