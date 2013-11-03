e9eb091692cb6292df2450baba27b7a2ebaa2886
Dotan Cohen <github-com-2010.04@dotancohen.com>
2013-11-03 13:25:13 +0200

    Added option to create hashed credentials lines

--- poor-mans-login.php
+++ poor-mans-login.php
@@ -1,6 +1,7 @@
 <?php
 
 // No database, no session!
+// Pass additional 'salt' parameter to get hashed credentials lines below.
 
 $salt = 'Some tasty salt';
 $usernameSha1 = 'fde3b9e88dca861202148dea3a7cec8b05befbc9';
@@ -10,6 +11,14 @@ $isAllowedIn  = FALSE;
 $secureIdentifier = sha1($salt.$_SERVER['HTTP_USER_AGENT']);
 
 if ( isset($_POST['username']) && isset($_POST['password']) ) {
+
+	if ( isset($_POST['salt']) ) {
+		header('Content-Type: plain/text');
+		echo "\$salt = '".$_POST['salt']."';\n";
+		echo "\$usernameSha1 = '".sha1($_POST['salt'].$_POST['username'])."';\n";
+		echo "\$passwordSha1 = '".sha1($_POST['salt'].$_POST['password'])."';\n";
+		exit();
+	}
 
 	setcookie('login', 'garbage', 0);
 
