2d8331f86591a3303912f53a617793e8fedba392
Dotan Cohen <github-com-2010.04@dotancohen.com>
2013-11-03 13:18:08 +0200

    Clarification comments

--- poor-mans-login.php
+++ poor-mans-login.php
@@ -9,12 +9,11 @@ $passwordSha1 = '85987fb0a90e77a0bdeeb5a2026fa8a4a67137f6';
 $isAllowedIn  = FALSE;
 $secureIdentifier = sha1($salt.$_SERVER['HTTP_USER_AGENT']);
 
-if ( isset($_POST['username']) || isset($_POST['password']) ) {
+if ( isset($_POST['username']) && isset($_POST['password']) ) {
 
 	setcookie('login', 'garbage', 0);
 
 	if ( sha1($salt.$_POST['username'])==$usernameSha1 && sha1($salt.$_POST['password'])==$passwordSha1 ) {
-		// Correct login credentials, set cookie and set pass variable
 		$sevenDays = time() + 7*24*60*60;
 		setcookie('login', $secureIdentifier, $sevenDays);
 		$_COOKIE['login'] = $secureIdentifier;
@@ -24,6 +23,9 @@ if ( isset($_POST['username']) || isset($_POST['password']) ) {
 } else if ( $_COOKIE['login']==$secureIdentifier ) {
 	$isAllowedIn = TRUE;
 }
+
+
+// From here check $isAllowedIn variable
 
 
 ?>
